0d88c6835b978276b4bf9dca733bfb399ae8c320
Soseeska <Soseeska@DESKTOP-N3EIT04>
2022-10-14 21:12:59 +0300

    Added DoctorRepository

---
+++ Hospital.WebAPI/Data/DoctorRepository.cs
@@ -0,0 +1,56 @@
+﻿using Hospital.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital.WebAPI.Data;
+
+public class DoctorRepository : IDoctorRepository
+{
+    private readonly HospitalContext _db;
+
+    public DoctorRepository(HospitalContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<IEnumerable<Doctor>> GetDoctorsAsync()
+    {
+        return await _db.Doctors.ToListAsync();
+    }
+
+    public async Task DeleteDoctorAsync(int id)
+    {
+        var doctor = await _db.Doctors.FindAsync(id);
+        if (doctor != null)
+            _db.Doctors.Remove(doctor);
+
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task AddDoctorAsync(Doctor doctor)
+    {
+        if (_db.Doctors.Any(x => x.FullName.Trim().Equals(doctor.FullName.Trim(), StringComparison.OrdinalIgnoreCase))) return;
+
+        _db.Doctors.Add(doctor);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task UpdateDoctorAsync(Doctor doctor)
+    {
+        if (_db.Doctors.Any(x => x.FullName.Trim().Equals(doctor.FullName.Trim(), StringComparison.OrdinalIgnoreCase))) return;
+
+        var doc = await _db.Doctors.FindAsync(doctor.Id);
+
+        if (doc != null)
+        {
+            doc.FullName = doctor.FullName;
+            doc.CabinetId = doctor.CabinetId;
+            doc.SpecializationId = doctor.SpecializationId;
+            doc.AreaId = doctor.AreaId;
+        }
+    }
+
+    public async Task<Doctor?> GetDoctorByIdAsync(int id)
+    {
+        return await _db.Doctors.FindAsync(id);
+    }
+}
---
+++ Hospital.WebAPI/Data/IDoctorRepository.cs
@@ -0,0 +1,13 @@
+﻿using Hospital.Shared;
+
+namespace Hospital.WebAPI.Data
+{
+    public interface IDoctorRepository
+    {
+        Task AddDoctorAsync(Doctor doctor);
+        Task DeleteDoctorAsync(int id);
+        Task<Doctor?> GetDoctorByIdAsync(int id);
+        Task<IEnumerable<Doctor>> GetDoctorsAsync();
+        Task UpdateDoctorAsync(Doctor doctor);
+    }
+}
