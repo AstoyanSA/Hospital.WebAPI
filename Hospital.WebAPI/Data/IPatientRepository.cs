b500b007d765cdc756066b91e354ecf15ba5d37b
Soseeska <Soseeska@DESKTOP-N3EIT04>
2022-10-14 21:00:41 +0300

    Added PatienRepository

---
+++ Hospital.WebAPI/Data/IPatientRepository.cs
@@ -0,0 +1,13 @@
+﻿using Hospital.Shared;
+
+namespace Hospital.WebAPI.Data
+{
+    public interface IPatientRepository
+    {
+        Task AddPatientAsync(Patient patient);
+        Task DeletePatientAsync(int id);
+        Task<Patient?> GetPatientByIdAsync(int id);
+        Task<IEnumerable<Patient>> GetPatientsAsync();
+        Task UpdatePatientAsync(Patient patient);
+    }
+}
---
+++ Hospital.WebAPI/Data/PatientRepository.cs
@@ -0,0 +1,64 @@
+﻿using Hospital.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital.WebAPI.Data;
+
+public class PatientRepository : IPatientRepository
+{
+    private readonly HospitalContext _db;
+
+    public PatientRepository(HospitalContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<IEnumerable<Patient>> GetPatientsAsync()
+    {
+        return await _db.Patients.ToListAsync();
+    }
+
+    public async Task DeletePatientAsync(int id)
+    {
+        var patient = await _db.Patients.FindAsync(id);
+        if (patient != null)
+            _db.Patients.Remove(patient);
+
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task AddPatientAsync(Patient patient)
+    {
+        if (_db.Patients.Any(x => x.FirstName.Equals(patient.FirstName, StringComparison.OrdinalIgnoreCase) &&
+                                  x.Surname.Equals(patient.Surname, StringComparison.OrdinalIgnoreCase) &&
+                                  x.MiddleName.Equals(patient.MiddleName, StringComparison.OrdinalIgnoreCase))) return;
+
+        _db.Patients.Add(patient);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task UpdatePatientAsync(Patient patient)
+    {
+        if (_db.Patients.Any(x => x.Id != patient.Id &&
+                             x.FirstName.Equals(patient.FirstName, StringComparison.OrdinalIgnoreCase) &&
+                             x.Surname.Equals(patient.Surname, StringComparison.OrdinalIgnoreCase) &&
+                             x.MiddleName.Equals(patient.MiddleName, StringComparison.OrdinalIgnoreCase))) return;
+
+        var pat = await _db.Patients.FindAsync(patient.Id);
+
+        if (pat != null)
+        {
+            pat.FirstName = patient.FirstName;
+            pat.Surname = patient.Surname;
+            pat.MiddleName = patient.MiddleName;
+            pat.Address = patient.Address;
+            pat.Birthday = patient.Birthday;
+            pat.Sex = patient.Sex;
+            pat.AreaId = patient.AreaId;
+        }
+    }
+
+    public async Task<Patient?> GetPatientByIdAsync(int id)
+    {
+        return await _db.Patients.FindAsync(id);
+    }
+}
